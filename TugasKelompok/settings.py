9b25dff83c9d456264202b3106f2eb420a672633
danktapir <rasyadponcosewoyo@gmail.com>
2021-10-18 17:41:01 +0700

    edit typo line 21 di settings.py

--- TugasKelompok/settings.py
+++ TugasKelompok/settings.py
@@ -18,7 +18,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-PRODUCTION = os.environ.get("DATABSE_URL") != None
+PRODUCTION = os.environ.get("DATABASE_URL") != None
 
 
 # Quick-start development settings - unsuitable for production
