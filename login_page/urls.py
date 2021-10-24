5180b09006313444b2d44f53d736d3069dd2777e
Ramdhan Firdaus Amelia <70691224+ramdhanfirdaus@users.noreply.github.com>
2021-10-24 15:48:28 +0700

    Add name='login'

--- login_page/urls.py
+++ login_page/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register', views.register_user),
-    path('login', views.login_user),
+    path('login', views.login_user, name="login"),
     path('home-page', views.home_page),
     path('logout', views.logout_user)
 ]
