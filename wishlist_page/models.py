8be693a90773c0433ad311aed37f046470acf655
David <david.alexander01@ui.ac.id>
2021-10-27 16:09:00 +0700

    wishlist page somewhat done

--- wishlist_page/models.py
+++ wishlist_page/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class WishlistItem(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    count = models.IntegerField()
