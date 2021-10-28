25da487c156c1ccf9c362d73eded9a19aaa17225
Kenzie Kyle Anderson <kenziekylea@gmail.com>
2021-10-29 00:17:18 +0700

    added models updated

--- product_list_page/models.py
+++ product_list_page/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class ProdukMasker(models.Model):
     nama = models.CharField(max_length=255)
-    rating = models.BigAutoField()
+    rating = models.BigIntegerField()
     deskripsi = models.CharField(max_length=255)
-    harga = models.BigAutoField()
-    stok = models.BigAutoField()
+    harga = models.BigIntegerField()
+    stok = models.BigIntegerField()
