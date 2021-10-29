88ec81cf9fb232dd8906d5b619fff15d414e8a7d
Ramdhan Firdaus Amelia <70691224+ramdhanfirdaus@users.noreply.github.com>
2021-10-29 11:27:54 +0700

    Add image model

--- product_list_page/models.py
+++ product_list_page/models.py
@@ -6,3 +6,15 @@ class ProdukMasker(models.Model):
     deskripsi = models.CharField(max_length=255)
     harga = models.BigIntegerField()
     stok = models.BigIntegerField()
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
