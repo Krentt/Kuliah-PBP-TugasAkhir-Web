604b3755516cf9a2522802e64eff434d4d26a3c2
Julius Prayoga Raka Nugroho <raka.gundam@hotmail.com>
2021-10-29 12:37:45 +0700

    Update checkout

--- cuztomize_masker_page/urls.py
+++ cuztomize_masker_page/urls.py
@@ -4,12 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 app_name = "cuztomize_masker_page"
+
 urlpatterns = [
     path('custom-mask/', custom_mask, name='custom_mask'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
