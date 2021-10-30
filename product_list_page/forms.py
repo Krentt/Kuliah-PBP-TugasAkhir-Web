6fec5c74fe2e6c45b4c9bb2753d1216f58e06b9f
Kenzie Kyle Anderson <kenziekylea@gmail.com>
2021-10-30 13:09:38 +0700

    updated models plp

--- product_list_page/forms.py
+++ product_list_page/forms.py
@@ -4,4 +4,31 @@ from .models import ProdukMasker
 class ProdukMaskerForm(forms.ModelForm):
     class Meta:
         model = ProdukMasker
-        fields = "__all__"
+        fields = ['nama', 'rating', 'deskripsi', 'harga', 'stok', 'image']
+        widgets = {
+            'nama' : forms.TextInput(attrs={
+                'class' : 'form-control',
+                'placeholder' : 'Masker Karbon Aktif',
+                'required' : ''
+            }),
+            'rating' : forms.NumberInput(attrs={
+                'class' : 'form-control',
+                'placeholder' : '90 (out of 100)',
+                'required' : ''
+            }),
+            'deskripsi' : forms.TextInput(attrs={
+                'class' : 'form-control',
+                'placeholder' : 'Masker wajah ini dapat mengatasi masalah kulit berminyak dan berjerawat',
+                'required' : ''
+            }),
+            'harga' : forms.NumberInput(attrs={
+                'class' : 'form-control',
+                'placeholder' : '10 (in dollars)',
+                'required' : ''
+            }),
+            'stok' : forms.NumberInput(attrs={
+                'class' : 'form-control',
+                'placeholder' : '1000',
+                'required' : '',
+            }),
+        }
