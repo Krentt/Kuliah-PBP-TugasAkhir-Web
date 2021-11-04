ab6e528715b420923c67b1b4db03fad311c0ca33
Ramdhan Firdaus Amelia <70691224+ramdhanfirdaus@users.noreply.github.com>
2021-11-04 15:24:50 +0700

    Update test.py

--- login_page/tests.py
+++ login_page/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import resolve
+from .views import *
+from .models import *
 
 # Create your tests here.
+
+class LoginTest(TestCase):
+
+    def setUp(self) -> None:
+        self.client = Client()
+
+    def test_home_page_is_exist(self):
+        response = self.client.get("/home-page")
+        self.assertEqual(response.status_code, 200)
+
+    def test_using_home_page_func(self):
+        found = resolve("/home-page")
+        self.assertEqual(found.func, home_page)
