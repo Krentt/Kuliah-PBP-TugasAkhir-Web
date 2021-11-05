647a5dd8f1f4031b33af29729d8e1b3912c9aa66
Julius Prayoga Raka Nugroho <raka.gundam@hotmail.com>
2021-11-05 19:45:14 +0700

    updateeee pls work pls

--- checkout_page/static/js/complete.js
+++ checkout_page/static/js/complete.js
@@ -1,21 +1,21 @@
-// $document.ready(function(){
-//     $(".calcprice").click(function(){
-//         $.ajax({
-//             url: 'checkout-4',
-//             type: 'GET',
-//             success: function(response) {
-//                 $(".btn").text(response.harga)
-//             }
-//         })
-//     })
-// })
+$document.ready(function(){
+    $(".calcprice").click(function(){
+        $.ajax({
+            url: 'checkout-4',
+            type: 'GET',
+            success: function(response) {
+                $(".calcprice").text(response.harga)
+            }
+        })
+    })
+})
 
 $('.calcprice').on('click', function() {
     $.ajax({
         url: 'checkout-4',
         type: 'GET',
         success: function(response) {
-            $(".btn").text(response.harga)
+            $(".calcprice").text(response.harga)
         }
     })
 })
