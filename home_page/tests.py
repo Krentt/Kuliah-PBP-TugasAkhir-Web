38134623bf1bb72fb59159a94805eeb035127e0b
Ramdhan Firdaus Amelia <70691224+ramdhanfirdaus@users.noreply.github.com>
2021-11-05 19:29:13 +0700

    update test

--- home_page/tests.py
+++ home_page/tests.py
@@ -1,3 +1,16 @@
+from django import http
+from django.http import response
 from django.test import TestCase
+from django.test import Client
+from django.urls import resolve
+from .views import *
+from django.contrib.auth.models import User
+from .models import *
+import time
 
 # Create your tests here.
+
+class Test(TestCase):
+    def test_testing_is_exist(self):
+        response = Client().get('/')
+        self.assertEqual(response.status_code, 200)
