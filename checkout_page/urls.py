245762456aeb79157d40d9b3d533c65b7e4d3827
Julius Prayoga Raka Nugroho <raka.gundam@hotmail.com>
2021-11-05 18:26:19 +0700

    update

--- checkout_page/urls.py
+++ checkout_page/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('checkout-2', views.checkout2_form),
     path('checkout-3', views.checkout3_form),
     path('checkout-4', views.checkout4),
-    path('calc', views.AjaxCalcPrice.as_view()),
+    path('calc', views.AjaxCalcPrice),
     path('checkout-complete', views.checkout_complete)
 ]
